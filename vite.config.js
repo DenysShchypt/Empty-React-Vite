25c18be82ba9d2194225c09717981a03d39b27da
DenysShchypt <denysshchyptsovit@gmail.com>
2024-03-03 15:37:43 +0200

    delete injectHTML()

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { injectHTML } from 'vite-plugin-html';
 import FullReload from 'vite-plugin-full-reload';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()
-    , injectHTML(), FullReload(['./**/**.html'])
+  plugins: [react(), FullReload(['./**/**.html'])
   ],
   server: {
     port: 3000,
