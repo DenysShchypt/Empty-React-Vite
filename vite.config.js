11fc166d7f7a193c34ab67e3c301aeb89d70c515
DenysShchypt <denysshchyptsovit@gmail.com>
2024-03-03 15:11:11 +0200

    delete root with config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
   plugins: [react()
     // , injectHTML(), FullReload(['./src/**/**.html'])
   ],
